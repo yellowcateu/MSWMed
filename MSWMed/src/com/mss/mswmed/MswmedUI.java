646f82885a67200c037b52373c03e43054ba76b6
tkowalski <tkowalski@DITK.MSWS.LOCAL>
2013-10-10 21:00:44 +0200

    changed theme and added class with login form

--- MSWMed/src/com/mss/mswmed/MswmedUI.java
+++ MSWMed/src/com/mss/mswmed/MswmedUI.java
@@ -3,18 +3,11 @@ package com.mss.mswmed;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.Title;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Panel;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.CustomLayout;
-import com.vaadin.ui.Notification;
-import com.vaadin.ui.Panel;
-import com.vaadin.ui.PasswordField;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.VerticalLayout;
 
 
 /**
@@ -27,46 +20,15 @@ public class MswmedUI extends UI {
 	
 	@Override
 	protected void init(VaadinRequest request) {
-		buildCustomForm();
-	}
-	
-	private void buildCustomForm(){
-		
 		VerticalLayout layout = new VerticalLayout();
 		setContent(layout);
 		layout.setSizeFull();
 		
+		MSWMedLoginForm lf = new MSWMedLoginForm();
+		Panel sample = lf.getPanel();
 		
-		Panel panel = new Panel();
-		panel.setSizeUndefined();
-
-		
-        CustomLayout sample = new CustomLayout("loginPanel");
-        panel.setContent(sample);
-        final TextField username = new TextField("U¿ytkownik");
-        username.setWidth("405");
-        username.setRequired(true);
-        username.setRequiredError("Pole nie mo¿e byæ puste");
-        sample.addComponent(username, "username");
-
-        final PasswordField password = new PasswordField("Has³o");
-        password.setRequired(true);
-        password.setRequiredError("Pole nie mo¿e byæ puste");
-        password.setWidth("405");
-        sample.addComponent(password, "password");
-
-        Button ok = new Button("Login");
-        
-        ok.addClickListener(new ClickListener() {
-        	public void buttonClick(ClickEvent event) {
-        	Notification.show("Login user " + username.getValue(), com.vaadin.ui.Notification.Type.WARNING_MESSAGE);
-        	}
-        	});
-        
-        sample.addComponent(ok, "okbutton");
-        
-        layout.addComponent(panel);
-        layout.setComponentAlignment(panel, Alignment.MIDDLE_CENTER);
+		layout.addComponent(sample);
+        layout.setComponentAlignment(sample, Alignment.MIDDLE_CENTER);
 	}
-
+	
 }
